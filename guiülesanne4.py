2ecd386fb35737938ed60bca13af2014d5139d9d
kristoferandres <45108172+kristoferandres@users.noreply.github.com>
2022-03-15 13:06:46 +0200

    Add files via upload

--- guiülesanne4.py
+++ guiülesanne4.py
@@ -3,47 +3,135 @@
 # Kristofer Andres
 
 from tkinter import *
+import math
+import time
 
 aken = Tk()
 aken.title('kalkulaator')
 aken.geometry("200x200+200+500")
+addlist = []
+vahesumma=0
+def num0():
+    if len(t.cget("text"))!=11:
+        t.config(text = t.cget("text") + "0")
+def num1():
+    if len(t.cget("text"))!=11:
+        t.config(text = t.cget("text") + "1")
+def num2():
+    if len(t.cget("text"))!=11:
+        t.config(text = t.cget("text") + "2")
+def num3():
+    if len(t.cget("text"))!=11:
+       t.config(text = t.cget("text") + "3")
+def num4():
+    if len(t.cget("text"))!=11:
+        t.config(text = t.cget("text") + "4")
+def num5():
+    if len(t.cget("text"))!=11:
+       t.config(text = t.cget("text") + "5")
+def num6():
+    if len(t.cget("text"))!=11:
+       t.config(text = t.cget("text") + "6")
+def num7():
+    if len(t.cget("text"))!=11:
+       t.config(text = t.cget("text") + "7")
+def num8():
+    if len(t.cget("text"))!=11:
+        t.config(text = t.cget("text") + "8")
+def num9():
+    if len(t.cget("text"))!=11:
+        t.config(text = t.cget("text") + "9")
+def coma():
+    if len(t.cget("text"))!=11:
+        t.config(text = t.cget("text") + ".")
+def add():
+    if t.cget("text") != '='+ str(vahesumma):
+        addlist.append(t.cget("text"))
+    addlist.append('+')
+    t.config(text='')
+def minus():
+    if t.cget("text") != '='+ str(vahesumma):
+        addlist.append(t.cget("text"))
+    addlist.append('-')
+    t.config(text='')
+def divide():
+    if t.cget("text") != '='+ str(vahesumma):
+        addlist.append(t.cget("text"))
+    addlist.append('/')
+    t.config(text='')
+def times():
+    if t.cget("text") != '='+ str(vahesumma):
+        addlist.append(t.cget("text"))
+    addlist.append('*')
+    t.config(text='')
+def equal():
+    global vahesumma
+    pikkus = len(addlist)/2
+    pikkus = math.ceil(pikkus)
+    addlist.append(t.cget("text"))
+    vahesumma=float(addlist[0])
+    for i in range(pikkus):
+        mark = addlist[i*2+1]
+        if mark== '+':
+            vahesumma = vahesumma + float(addlist[i*2+2])
+        if mark== '-':
+            vahesumma = vahesumma - float(addlist[i*2+2])
+        if mark== '/':
+            vahesumma = vahesumma / float(addlist[i*2+2])
+        if mark== '*':
+            vahesumma = vahesumma * float(addlist[i*2+2])
+    t.config(text='='+ str(vahesumma))
+    
 
-cs = 4
-v = cs
 
-text = Label(aken, text='tech', font="Tahoma 12", justify=CENTER).grid(row=1, column=0, padx= 2, pady= 2, columnspan=cs*4, sticky=E+W)
-r = v * 1
-num7 = Button(aken, text="7", font="Tahoma 12").grid(row=2, column=1, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 1
-num4 = Button(aken, text="4", font="Tahoma 12").grid(row=3, column=1, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 1
-num1 = Button(aken, text="1", font="Tahoma 12").grid(row=4, column=1, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 1
-num0 = Button(aken, text="0", font="Tahoma 12").grid(row=5, column=1, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 2
-num8 = Button(aken, text="8", font="Tahoma 12").grid(row=2, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 2
-num5 = Button(aken, text="5", font="Tahoma 12").grid(row=3, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 2
-num2 = Button(aken, text="2", font="Tahoma 12").grid(row=4, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 2
-coma = Button(aken, text=",", font="Tahoma 12").grid(row=5, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 3
-num9 = Button(aken, text="9", font="Tahoma 12").grid(row=2, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 3
-num6 = Button(aken, text="6", font="Tahoma 12").grid(row=3, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 3
-num3 = Button(aken, text="3", font="Tahoma 12").grid(row=4, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W) 
-r = v * 3
-equal = Button(aken, text="=", font="Tahoma 12").grid(row=5, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 4
-divide = Button(aken, text="/", font="Tahoma 12").grid(row=2, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 4
-times = Button(aken, text="*", font="Tahoma 12").grid(row=3, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 4
-minus = Button(aken, text="-", font="Tahoma 12").grid(row=4, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
-r = v * 4
-add = Button(aken, text="+", font="Tahoma 12").grid(row=5, column=r, padx= 2, pady= 2, columnspan=cs, sticky=E+W)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+t = Label(aken, text='', font="Tahoma 12")
+t.grid(row=1, column=0, padx= 2, pady= 2, columnspan=4)
+num7 = Button(aken, text="7", font="Tahoma 12", command=num7)
+num7.grid(row=2, column=1, padx= 2, pady= 2)
+num4 = Button(aken, text="4", font="Tahoma 12", command=num4)
+num4.grid(row=3, column=1, padx= 2, pady= 2)
+num1 = Button(aken, text="1", font="Tahoma 12", command=num1)
+num1.grid(row=4, column=1, padx= 2, pady= 2)
+num0 = Button(aken, text="0", font="Tahoma 12", command=num0)
+num0.grid(row=5, column=1, padx= 2, pady= 2)
+num8 = Button(aken, text="8", font="Tahoma 12", command=num8)
+num8.grid(row=2, column=2, padx= 2, pady= 2)
+num5 = Button(aken, text="5", font="Tahoma 12", command=num5)
+num5.grid(row=3, column=2, padx= 2, pady= 2)
+num2 = Button(aken, text="2", font="Tahoma 12", command=num2)
+num2.grid(row=4, column=2, padx= 2, pady= 2)
+coma = Button(aken, text=",", font="Tahoma 12", command=coma)
+coma.grid(row=5, column=2, padx= 2, pady= 2)
+num9 = Button(aken, text="9", font="Tahoma 12", command=num9)
+num9.grid(row=2, column=3, padx= 2, pady= 2)
+num6 = Button(aken, text="6", font="Tahoma 12", command=num6)
+num6.grid(row=3, column=3, padx= 2, pady= 2)
+num3 = Button(aken, text="3", font="Tahoma 12", command=num3)
+num3.grid(row=4, column=3, padx= 2, pady= 2) 
+equal = Button(aken, text="=", font="Tahoma 12", command=equal)
+equal.grid(row=5, column=3, padx= 2, pady= 2)
+divide = Button(aken, text="/", font="Tahoma 12", command=divide)
+divide.grid(row=2, column=4, padx= 2, pady= 2)
+times = Button(aken, text="*", font="Tahoma 12", command=times)
+times.grid(row=3, column=4, padx= 2, pady= 2)
+minus = Button(aken, text="-", font="Tahoma 12", command=minus)
+minus.grid(row=4, column=4, padx= 2, pady= 2)
+add = Button(aken, text="+", font="Tahoma 12", command=add)
+add.grid(row=5, column=4, padx= 2, pady= 2)
 
 
 
--- guiülesanne6.py
+++ guiülesanne6.py
@@ -6,11 +6,10 @@ from tkinter import *
 
 aken1 = Tk()
 aken1.title('Lipp')
-aken2 = Tk()
-aken2.title('Pilt')
 
 
-c = Canvas(aken1, width=300, height=200)
+
+c = Canvas(aken1, width=300, height=400)
 c.pack()
 
 c.create_rectangle(0,100,300,200, fill="#be0026", outline= '#be0026')
@@ -18,5 +17,7 @@ c.create_oval(56,43,178,165, fill="white", outline= 'white')
 c.create_rectangle(0,0,300,100, fill="white", outline= 'white')
 c.create_arc(56, 42, 178, 164, extent=180, fill="#be0026", outline= '#be0026')
 
+minu_pilt = PhotoImage(file='greenland.gif')
+c.create_image(0, 200, anchor=NW, image=minu_pilt)
+
 aken1.mainloop()
-aken2.mainloop()
